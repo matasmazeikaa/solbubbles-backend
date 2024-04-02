575519a81fd98df3a0568846a1ee337d4c67270d
Matas <matasmazeika@gmail.com>
2024-04-02 06:23:59 +0300

    add envs

--- src/supabasedb.ts
+++ src/supabasedb.ts
@@ -1,18 +1,23 @@
-import { SupabaseClient, createClient } from '@supabase/supabase-js'
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { Database } from 'database.types';
 
-export let supabase:SupabaseClient;
+export let supabase: SupabaseClient;
 
 export const initSupabase = async () => {
-    try {
-        supabase = await createClient(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLIC_KEY)
+	try {
+		supabase = await createClient(
+			process.env.SUPABASE_URL,
+			process.env.SUPABASE_PUBLIC_KEY
+		);
 
-        console.log('supabase init successfuly')
-    } catch (error) {
-        console.log('failed to init supabase client')
-    }
-}
+		console.log('supabase init successfuly');
+	} catch (error) {
+		console.log('failed to init supabase client');
+	}
+};
 
-export const getServiceClient = () => {
-    return createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY)
-}
+export const getServiceClient = () =>
+	createClient<Database>(
+		process.env.SUPABASE_URL,
+		process.env.SUPABASE_SERVICE_KEY
+	);
